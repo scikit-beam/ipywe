432d05d09708cb864afc0fd5e2168d7d580f13a6
Ian Lumsden <lumsden.ian@gmail.com>
2017-06-27 09:13:36 -0400

    Ref #11. Added the setup.py file to the ian-img-display branch.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ipywe",
+    version = "0.1",
+    packages = find_packages(exclude=["tests"]),
+    package_dir = {'': "."},
+    test_suite = "tests",   
+    install_requires = [
+        "numpy",
+        "ipywidgets",
+        "traitlets",
+    ],
+    package_data = {
+        '': ["*.js"],
+    },
+    dependency_links = [
+    ],
+    author = "ipywe team",
+    description = "Widgets for iMars3D",
+    license = 'BSD',
+    keywords = [
+        "neutron imaging",
+        "ipywidgets",
+        "jupyter widgets",
+    ],
+    url = "https://github.com/neutrons/ipywe",
+)
