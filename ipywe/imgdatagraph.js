be8307d2f9c97a2711ed697fc13576448ecb30d2
Ian Lumsden <lumsden.ian@gmail.com>
2017-07-11 12:59:43 -0400

    First full version of the code for graphing the data along a line from an image. This code has not been debugged yet, so errors do exist.

--- ipywe/imgdatagraph.js
+++ ipywe/imgdatagraph.js
@@ -6,13 +6,86 @@ define("imgdatagraph", ["jupyter-js-widgets"], function(widgets) {
     var ImgDataGraphView = widgets.DOMWidgetView.extend({
 
         render: function() {
-            var img_container = $('<div class="img-container">');
-            img_container.width(this.model.get("width")); img_container.height(this.model.get("height"));
-            this.$el.append(img_container);
+            var wid = this;
 
+            var img_container = $('<div class="img-container">');
+            this.$el.append(img_container);
+            img_container.width(this.model.get("width")); img_container.height(this.model.get("height"));
+
+            var img = $('<img class="curr-img">');
+            var image_src = "data:image/" + this.model.get("_format") + ";base64," + this.model.get("_b64value")
+            img.attr("src", image_src);
+            img_container.append(img);
+            img.css("position", "absolute");
+            img.width(this.model.get("width")); img.height(this.model.get("height"));
+
+            var canvas = $('<canvas class="img-canvas">');
+            canvas.width(this.model.get("width")); canvas.height(this.model.get("height"));
+            canvas.css("position", "absolute");
+            img_container.append(canvas);
+            var can = canvas[0];
+            var ctx = can.getContext('2d');
+
+            img.on("dragstart", false);
             
+            canvas.on("mousedown", function(event) {
+                console.log("mousedown");
+                ctx.clearRect(0, 0, wid.model.get("width"), wid.model.get("height"));
+                var offx = event.offsetX;
+                var offy = event.offsetY;
+                wid.model.set("_offsetX1", offx);
+                wid.model.set("_offsetY1", offy);
+                wid.touch();
+                ctx.moveTo(offx, offy);
+                canvas.on("mousemove", function(event) {
+                    console.log("mousemove");
+                    var currx = event.offsetX;
+                    var curry = event.offsetY;
+                    wid.model.set("_offsetX2", currx);
+                    wid.model.set("_offsetY2", curry);
+                    wid.touch();
+                    ctx.lineWidth = 5;
+                    ctx.strokeStyle = "#ff0000";
+                    ctx.beginPath();
+                    ctx.lineTo(currx, curry);
+                    ctx.stroke();
+                }).on("mouseup", function(event) {
+                    console.log("mouseup");
+                    canvas.off("mousedown");
+                });
+            });
+
+            /*var graph_button = $('<button class="graph-button">');
+            graph_button.button({
+                label: "Graph",
+                disabled: false
+            });
+            this.$el.append(graph_button);
+            graph_button.css("margin", "10px");
+            graph_button.click(function() {
+                var graph_val = wid.model.get("_graph_click");
+                if (graph_val < Number.MAX_SAFE_INTEGER) {
+                    graph_val++;
+                }
+                else {
+                    graph_val = 0;
+                }
+                wid.model.set("_graph_click", graph_val);
+                wid.touch();
+                ctx.clearRect(0, 0, wid.model.get("width"), wid.model.get("height"));
+            });*/
+            this.model.on("change:_b64value", this.on_img_change, this);
         },
 
+        on_img_change: function() {
+            var src = "data:image/" + this.model.get("_format") + ";base64," + this.model.get("_b64value");
+            this.$el.find(".curr-img").attr("src", src);
+        }
+
     });
+
+    return {
+        ImgDataGraphView : ImgDataGraphView
+    };
 
 });
--- ipywe/imgdatagraph.py
+++ ipywe/imgdatagraph.py
@@ -3,7 +3,8 @@ import ipywidgets as ipyw
 from IPython.display import display, HTML, clear_output
 from cStringIO import StringIO
 import sys, os
-from traitlets import Unicode, Integer, Float, HasTriats, observe
+from traitlets import Unicode, Integer, Float, HasTraits, observe
+import matplotlib.pyplot as plt
 
 
 class ImageDataGraph(ipyw.DOMWidget):
@@ -15,10 +16,13 @@ class ImageDataGraph(ipyw.DOMWidget):
     _format = Unicode().tag(sync=True)
     _nrows = Integer().tag(sync=True)
     _ncols = Integer().tag(sync=True)
-    _offsetX = Float().tag(sync=True)
-    _offsetY = Float().tag(sync=True)
+    _offsetX1 = Float().tag(sync=True)
+    _offsetY1 = Float().tag(sync=True)
+    _offsetX2 = Float().tag(sync=True)
+    _offsetY2 = Float().tag(sync=True)
     _img_min = Float().tag(sync=True)
     _img_max = Float().tag(sync=True)
+    _graph_click = Integer(0).tag(sync=True)
     
     width = Integer().tag(sync=True)
     height = Integer().tag(sync=True)
@@ -50,5 +54,57 @@ class ImageDataGraph(ipyw.DOMWidget):
         f = StringIO()
         import PIL.Image, base64
         PIL.Image.fromarray(img).save(f, self._format)
-        imgb64v = base64.b64encode(f.getValue())
+        imgb64v = base64.b64encode(f.getvalue())
         return imgb64v
+
+    @observe("_graph_click")
+    def graph_data(self, change):
+        p1x_abs = self._offsetX1*1./self.width * self._ncols
+        p1y_abs = self._offsetY1*1./self.height * self._nrows
+        p2x_abs = self._offsetX2*1./self.width * self._ncols
+        p2y_abs = self._offsetY2*1./self.height * self._nrows
+        slope = abs(p2y - p1y) / abs(p2x - p1x)
+        xcoords = []
+        ycoords = []
+        dists = []
+        vals = []
+        curr_x_abs = p1x_abs
+        curr_y_abs = p1y_abs
+        curr_x = int(curr_x_abs)
+        curr_y = int(curr_y_abs)
+        xcoords.append(curr_x)
+        ycoords.append(curr_y)
+        vals.append(self.img_data[curr_x, curr_y])
+        while curr_x_abs < p2x_abs and curr_y_abs < p2y_abs:
+            curr_x_abs += 1
+            curr_y_abs += slope
+            curr_x = int(curr_x_abs)
+            curr_y = int(curr_y_abs)
+            if curr_x_abs < p2x_abs and curr_y_abs < p2y_abs:
+                xcoords.append(curr_x)
+                ycoords.append(curr_y)
+                vals.append(self.img_data[curr_x, curr_y])
+        curr_x = int(p2x_abs)
+        curr_y = int(p2y_abs)
+        xcoords.append(curr_x)
+        ycoords.append(curr_y)
+        vals.append(self.img_data[curr_x, curr_y])
+        for x, y in np.nditer([xcoords, ycoords]):
+            dist = np.sqrt(((x - xcoords[0])**2 + (y - ycoords[0])**2))
+            dists.append(dist)
+        plt.plot(dists, vals)
+        plt.axis([int(np.min(dists)) * 0.75, int(np.max(dists)), int(np.min(vals)) * 0.75, int(np.max(vals))])
+        plt.xlabel("Distance from Initial Point")
+        plt.ylabel("Value")
+        plt.show()
+        return
+
+def get_js():
+    js = open(os.path.join(os.path.dirname(__file__), "imgdatagraph.js")).read()
+    return js.decode("UTF-8")
+
+def run_js():
+    js = get_js()
+    display(HTML("<script>"+js+"</script>"))
+
+run_js()
