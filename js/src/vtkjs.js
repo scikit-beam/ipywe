bc615dcfabe4418230a36bfaef469f09c5adfc09
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-11-09 17:07:03 -0500

    Work in progress - implementing arbitrary file load for vtkjs.

--- js/src/vtkjs.js
+++ js/src/vtkjs.js
@@ -44,27 +44,73 @@ var VtkJsView = widgets.DOMWidgetView.extend({
             renderWindow.addRenderer(renderer);
 
             // var fullScreenRenderer = vtk.Rendering.Core.vtkRenderWindow.newInstance();
-            var actor              = vtk.Rendering.Core.vtkActor.newInstance();
-            var mapper             = vtk.Rendering.Core.vtkMapper.newInstance();
-            var cone               = vtk.Filters.Sources.vtkConeSource.newInstance();
-            actor.setMapper(mapper);
-            mapper.setInputConnection(cone.getOutputPort());
-            renderer.addActor(actor);
-            renderer.resetCamera();
+            // var actor              = vtk.Rendering.Core.vtkActor.newInstance();
+            // var mapper             = vtk.Rendering.Core.vtkMapper.newInstance();
+            // var cone               = vtk.Filters.Sources.vtkConeSource.newInstance();
 
-            // Sources/Rendering/OpenGL/RenderWindow
+            $.get('/files/data/head-binary.vti', function(fileContentAsText){
+                var vtiReader = vtk.IO.XML.vtkXMLImageDataReader.newInstance();
+                vtiReader.parse(fileContentAsText);
 
-            var openglRenderWindow = vtk.Rendering.OpenGL.vtkRenderWindow.newInstance();
-            renderWindow.addView(openglRenderWindow);
+                var source = vtiReader.getOutputData(0);
+                console.log(source);
+                var mapper = vtk.Rendering.Core.vtkVolumeMapper.newInstance();
+                var actor = vtk.Rendering.Core.vtkVolume.newInstance();
 
-            openglRenderWindow.setContainer(container);
+                var dataArray = source.getPointData().getScalars() || source.getPointData().getArrays()[0];
+                var dataRange = dataArray.getRange();
+                var lookupTable = vtk.Rendering.Core.vtkColorTransferFunction.newInstance();
+                var piecewiseFunction = vtk.Common.DataModel.vtkPiecewiseFunction.newInstance();
 
-            renderWindow.render();
+                actor.setMapper(mapper);
+                mapper.setInputData(source);
+                renderer.addActor(actor);
+                renderer.resetCamera();
 
-            var interactor =     vtk.Rendering.Core.vtkRenderWindowInteractor.newInstance();
-            interactor.setView(openglRenderWindow);
-            interactor.initialize();
-            interactor.bindEvents(container);
+                var sampleDistance = 0.7 * Math.sqrt(source.getSpacing().map(v => v * v).reduce((a, b) => a + b, 0));
+                console.log('sampleDistance', sampleDistance);
+                mapper.setSampleDistance(sampleDistance);
+                actor.getProperty().setRGBTransferFunction(0, lookupTable);
+                actor.getProperty().setScalarOpacity(0, piecewiseFunction);
+                // actor.getProperty().setInterpolationTypeToFastLinear();
+                actor.getProperty().setInterpolationTypeToLinear();
+                // For better looking volume rendering
+                // - distance in world coordinates a scalar opacity of 1.0
+                actor.getProperty().setScalarOpacityUnitDistance(0, vtk.Common.DataModel.vtkBoundingBox.getDiagonalLength(source.getBounds()) / Math.max(...source.getDimensions()));
+                // - control how we emphasize surface boundaries
+                //  => max should be around the average gradient magnitude for the
+                //     volume or maybe average plus one std dev of the gradient magnitude
+                //     (adjusted for spacing, this is a world coordinate gradient, not a
+                //     pixel gradient)
+                //  => max hack: (dataRange[1] - dataRange[0]) * 0.05
+                actor.getProperty().setGradientOpacityMinimumValue(0, 0);
+                actor.getProperty().setGradientOpacityMaximumValue(0, (dataRange[1] - dataRange[0]) * 0.05);
+                // - Use shading based on gradient
+                actor.getProperty().setShade(true);
+                actor.getProperty().setUseGradientOpacity(0, true);
+                // - generic good default
+                actor.getProperty().setGradientOpacityMinimumOpacity(0, 0.0);
+                actor.getProperty().setGradientOpacityMaximumOpacity(0, 1.0);
+                actor.getProperty().setAmbient(0.2);
+                actor.getProperty().setDiffuse(0.7);
+                actor.getProperty().setSpecular(0.3);
+                actor.getProperty().setSpecularPower(8.0);
+
+
+                // Sources/Rendering/OpenGL/RenderWindow
+
+                var openglRenderWindow = vtk.Rendering.OpenGL.vtkRenderWindow.newInstance();
+                renderWindow.addView(openglRenderWindow);
+
+                openglRenderWindow.setContainer(container);
+
+                renderWindow.render();
+
+//                var interactor = vtk.Rendering.Core.vtkRenderWindowInteractor.newInstance();
+//                interactor.setView(openglRenderWindow);
+//                interactor.initialize();
+//                interactor.bindEvents(container);
+            });
         });
     }
 });
